c9a5200a63bcbf0f13d80b51072692bd0e3f5cc5
Frank Johnson & Jim Patt <student@epicodus.com>
2015-06-11 16:49:36 -0700

    refactor router.js

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 StatsKeeper.Router.map(function() {
-  this.resource('teams', {path: '/'});
-  this.resource('new-team');
+  this.resource('teams', {path: '/'}, function() {
+    this.resource('new-team');
+  });
   this.resource('team', {path: 'teams/:team_id'}, function() {
     this.resource('new-player');
   });
